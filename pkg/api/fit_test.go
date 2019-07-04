b74bb41037e5c18469b638217e82334a09b02b95
Christopher Meis <christopher.meis@9elements.com>
2019-07-04 15:50:51 +0200

    Fixed go_test.go so it works with changes from last commit

--- pkg/api/fit_test.go
+++ pkg/api/fit_test.go
@@ -1,12 +1,16 @@
 package api
 
 import (
+	"golang.org/x/exp/mmap"
 	"testing"
 )
 
 func TestExtractFit(t *testing.T) {
-
-	fitTable, err := ExtractFit("/home/riot/work_stuff/coreboot_wege100s_systemboot_tboot.rom")
+	file, err := mmap.Open("/home/riot/work_stuff/coreboot_wege100s_systemboot_tboot.rom")
+	filesize := file.Len()
+	data := make([]byte, filesize)
+	file.ReadAt(data, 0)
+	fitTable, err := ExtractFit(data)
 	if err != nil {
 		t.Errorf("ExtractFit() failed: %v", err)
 	}
